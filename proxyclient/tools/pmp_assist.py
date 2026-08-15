c587ae6d50cf29e5932b3455610ac0fffa2ee3e8
Sasha Finkelstein <k@chaosmail.tech>
2026-08-15 22:07:03 +0200

    proxyutils/pmp: Parse more pmp entries
    Signed-off-by: Sasha Finkelstein <k@chaosmail.tech>

--- proxyclient/tools/pmp_assist.py
+++ proxyclient/tools/pmp_assist.py
@@ -23,6 +23,9 @@ for i in range(len(pmp_ptd_range) // 32):
     id, offset, _, name = struct.unpack('<II8s16s', pmp_ptd_range[i*32:(i+1)*32])
     pmp_ptd_range_map[name.strip(b'\x00')] = offset
 
+pmp_node = dt['/arm-io/pmp']
+pmp_base = pmp_node.get_reg(pmp_node.ptd_update_reg_index)[0] - 0x10000
+print("PMP base", hex(pmp_base))
 print("DEV_STATUS_TGT_RD:", hex(pmp_ptd_range_map[b'SOC-DEV-PS-REQ'] * 16))
 print("DEV_STATUS_TGT_WR:", hex(pmp_ptd_range_map[b'SOC-DEV-PS-REQ'] * 8 + 0x10000))
 print("DEV_STATUS_ACT:", hex(pmp_ptd_range_map[b'SOC-DEV-PS-ACK'] * 16))
@@ -36,3 +39,34 @@ for dev in sorted(dt['/arm-io/pmgr'].devices, key=lambda dev: dev.id1):
     if dev.flags.notify_pmp:
         s += " (notify_pmp)"
     print(s)
+
+soc_dev = dt['/arm-io/pmp/iop-pmp-nub'].soc_device
+bw_dev = {}
+j = 0
+for i in range(len(soc_dev) // 124):
+    id, _, has_bw_req, _, name = struct.unpack('<I44sI64s8s', soc_dev[i*124:(i+1)*124])
+    if has_bw_req:
+        bw_dev[id] = j
+        j += 1
+
+print()
+print('bw-scratch:')
+pmgr_devs = {x.id2:x for x in dt['/arm-io/pmgr'].devices}
+for seg in ['disp', 'dispext', 'isp']:
+    i = 0
+    while 1:
+        name = f'{seg}{i}'
+        path = f'/arm-io/{name}'
+        if path not in dt:
+            break
+        node = dt[path]
+        i += 1
+        for sfx in ['', '0']:
+            prop = node._properties.get(f'function-bw_req_interrupt{sfx}')
+            if prop is None:
+                continue
+            pmgr_id = prop.args[0]
+            pmp_id = pmgr_devs[pmgr_id].id1
+            offs = (bw_dev[pmp_id] + pmp_ptd_range_map[b'SOC-DEV-BWR']) * 8 + 0x10000 + pmp_base
+            print(f'\t{name} {hex(offs)}')
+            break
